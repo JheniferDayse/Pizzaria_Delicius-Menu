7a762055166d3b5a234c21ae3e2bc5c33266b48a
Jhenifer <jheniferdayse@gmail.com>
2022-12-30 17:37:45 -0300

    primeiro commit

---
+++ pizzas.js
@@ -0,0 +1,122 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Mussarela',
+        img: 'imagens/pizza.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, camada dupla de mussarela e orégano'
+      },
+      {
+        id: 2,
+        name: 'Calabresa',
+        img: 'imagens/pizza2.png',
+        price: [21.00, 24.00, 26.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+      },
+      {
+        id: 3,
+        name: 'Quatro Queijos',
+        img: 'imagens/pizza3.png',
+        price: [23.00, 26.00, 28.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+      },
+      {
+        id: 4,
+        name: 'Baiana',
+        img: 'imagens/pizza4.png',
+        price: [25.00, 28.00, 30.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+      },
+      {
+        id: 5,
+        name: 'Portuguesa',
+        img: 'imagens/pizza5.png',
+        price: [24.00, 27.00, 29.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+      },
+      {
+        id: 6,
+        name: 'Moda da Casa',
+        img: 'imagens/pizza6.png',
+        price: [30.00, 33.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+      },
+      {
+        id: 7,
+        name: 'Banana com doce de leite',
+        img: 'imagens/pizza7.png',
+        price: [27.00, 30.00, 32.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'banana e doce de leite'
+      },
+      {
+        id: 8,
+        name: 'Chocolate com morango',
+        img: 'imagens/pizza8.png',
+        price: [30.00, 32.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      },
+      {
+        id: 9,
+        name: 'Adicional de cebola',
+        img: 'imagens/cebolas.png',
+        price: [2.00, 4.00, 6.00],
+        sizes: [
+          '1 leve',
+          '2 moderado',
+          '3 bastante'
+        ],
+        description: 'Cebolas cortadas em rodelas'
+      },
+      {
+        id: 10,
+        name: 'Adicional de catupiry',
+        img: 'imagens/catupiry.png',
+        price: [2.00, 4.00, 6.00],
+        sizes: [
+          '1 leve',
+          '2 moderado',
+          '3 bastante'
+        ],
+        description: 'Adicional de catupiry original'
+      }
+];
